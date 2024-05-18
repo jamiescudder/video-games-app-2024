b357d920af920f618381083b4efbc0de769a8d99
Jamie Scudder <jamie.scudder@hotmail.com>
2024-05-18 11:19:02 +0100

    Create text component

---
+++ src/components/text.tsx
@@ -0,0 +1,38 @@
+import styled from 'styled-components'
+import {
+  SpaceProps,
+  LayoutProps,
+  PositionProps,
+  TypographyProps,
+  BorderProps,
+  ColorProps,
+  space,
+  layout,
+  position,
+  typography,
+  border,
+  color,
+} from 'styled-system'
+
+export interface Props
+  extends SpaceProps,
+    LayoutProps,
+    PositionProps,
+    TypographyProps,
+    BorderProps,
+    ColorProps {
+  children?: React.ReactNode
+}
+
+const Text = styled.p<Props>`
+  font-size: var(--p-font-size);
+  line-height: 1.5;
+
+  ${layout}
+  ${space}
+  ${position}
+  ${typography}
+  ${border}
+  ${color}
+`
+export default Text
